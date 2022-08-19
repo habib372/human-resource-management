6112ef629d326976a0b5f5eb575c031eae6fffc1
Habibur Rahman (habib) <54948423+habib372@users.noreply.github.com>
2022-08-19 22:38:51 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ use App\Http\Controllers\UserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 //----normal routing without controller------> 
 // Route::get('/', function () {
 //     return view('include.login');
